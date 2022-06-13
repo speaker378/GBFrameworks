76f16839816d6dc02872f3054446bb813c102239
speaker378 <speaker378debua@gmail.com>
2022-06-13 21:35:56 +0500

    Initial Commit

---
+++ GBFrameworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBFrameworks
+//
+//  Created by Сергей Черных on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

bfb8b36a74f4f0ff1c9b372b232df6cfc72338db
speaker378 <speaker378debua@gmail.com>
2022-06-22 17:15:38 +0500

    4. Добавьте контроллер для входа, где можно ввести логин и пароль, а также воспользоваться кнопками «Вход» и «Регистрация».

---
+++ GBFrameworks/App/Flows/Auth/AuthViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthViewController.swift
+//  GBFrameworks
+//
+//  Created by Сергей Черных on 22.06.2022.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func loginButtonTap(_ sender: UIButton) { }
+    @IBAction func registrationButtonTap(_ sender: UIButton) { }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

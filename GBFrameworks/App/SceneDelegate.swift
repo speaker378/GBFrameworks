88b9fa0ed5a6a5b955a8681215fed311b2b2e9ea
speaker378 <speaker378debua@gmail.com>
2022-06-24 15:40:42 +0500

    2. Настроить показ шторки при уходе приложения в фоновый режим.

--- GBFrameworks/App/SceneDelegate.swift
+++ GBFrameworks/App/SceneDelegate.swift
@@ -11,6 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var coordinator: AppCoordinator?
+    let blindView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -29,13 +30,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        blindView.removeFromSuperview()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        guard let window = window else { return }
+        blindView.frame = window.bounds
+        window.rootViewController?.view.addSubview(blindView)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
